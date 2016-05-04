0e038a54c19fcc5b18e1c4ce1b5e838e8d82a760
Samarth Mohan <samohan@dhcp-18-111-84-151.dyn.mit.edu>
2016-05-03 23:43:33 -0400

    Dynamically loading meters, changed car owner directions, UI changes

--- js/officialListView.js
+++ js/officialListView.js
@@ -1,51 +1,91 @@
 (function () {
 	var meterListings = new Firebase("https://streetparkofficialdata.firebaseio.com/");
+	var meters = meterListings.child("meters");
+	var listOfExpiredMeters = [];
+	var listOfWarningMeters = [];
 
 	function retrieveDataAndPopulate() {
-		var listOfMeters = [];
+		var listOfMeters = [], parkingLotAddresses = [];
 		var dMatrix = new google.maps.DistanceMatrixService();
-		console.log(dMatrix);
+		// get all meters from firebase
 		meterListings.on("value", function (snapshot) {
 			for (var meter in snapshot.val().meters) {
-				listOfMeters.push(snapshot.val().meters[meter]);
+				var meterObj = snapshot.val().meters[meter];
+				meterObj.id = meter;
+				listOfMeters.push(meterObj);
 			}
-			console.log(listOfMeters);
+			// list of parking lot addresses
+			listOfMeters.forEach(function (element) {
+				parkingLotAddresses.push(element.address);
+			});
+			var officialOriginAddress = sessionStorage.getItem("officialOriginAddress");
+			dMatrix.getDistanceMatrix({
+				origins: [officialOriginAddress],
+				destinations: parkingLotAddresses,
+				travelMode: google.maps.TravelMode.DRIVING,
+				unitSystem: google.maps.UnitSystem.IMPERIAL,
+				avoidHighways: false,
+				avoidTolls: false 
+			}, function (response, status) {
+				if (status === google.maps.DistanceMatrixStatus.OK) {
+					var origin = response.originAddresses;
+					var destination = response.destinationAddresses;
+					var responseList = response.rows[0].elements;
+					for (var i=0; i < responseList.length; i++) {
+						var currentItem = responseList[i];
+						listOfMeters[i].distance = parseFloat(currentItem.distance.text);
+						if (Math.floor(($.now() - listOfMeters[i].createdAt)/ 60000) > parseInt(listOfMeters[i].timeRemaining)) {
+							// meter has expired
+							listOfExpiredMeters.push(listOfMeters[i]);
+						} else {
+							listOfWarningMeters.push(listOfMeters[i]);
+						}
+					}
+					console.log(listOfExpiredMeters, listOfWarningMeters);
+					populateExpiredTable(listOfExpiredMeters);
+					populateWarningTable(listOfWarningMeters)
+				}
+			});
 		});
 	}
 
-	function populateTable() {
-		var tableElements = [];
-		if (sessionStorage.getItem('tableElements') === null) {
-			var values = [
-			{"address": "9 Beacon St.", "meternum": "Meter #2722"},
-			{"address": "24 Tremont St.", "meternum": "Meter #336"},
-			{"address": "36 Berkeley St.", "meternum": "Meter #712"},
-			{"address": "117 A St.", "meternum": "Meter #1098"}
-			];
-			tableElements = JSON.stringify(values);
-			sessionStorage.setItem('tableElements', tableElements);
-		} else {
-			tableElements = sessionStorage.getItem('tableElements');
-		}
-		// check if tableElements is empty or not
-		var parsedTableElements = JSON.parse(tableElements);
-		if (parsedTableElements.length === 0) {
-			var h1 = $("<h1 id='emptyListHeading'>No Expired Meters</h1>");
+	function populateExpiredTable(expiredMeters) {
+		// check if expiredtableElements is empty or not
+		if (expiredMeters.length === 0) {
+			var h1 = $("<h1 class='emptyListHeading'>No Expired Meters</h1>");
 			$("#expiredTableBody").append(h1);
 		} else {
-			for (var i=0; i<parsedTableElements.length; i++) {
-				var row = $("<tr id="+i+" class='officialItem' data-href='expiredMeter.html'><td data-th='Address'>" + parsedTableElements[i].address +"</td><td data-th='Meter #'>" + parsedTableElements[i].meternum + "</td></tr>");
+			for (var i=0; i<expiredMeters.length; i++) {
+				var row = $("<tr class='officialItem' data-href='expiredMeter.html'><td data-th='Address'>" + expiredMeters[i].name +"</td><td data-th='distance'>" + expiredMeters[i].distance + " mi</td></tr>");
 				$("#expiredTableBody").append(row);
 			}
 		}
 	}
+
+	function populateWarningTable(warningMeters) {
+		if (warningMeters.length === 0) {
+			var h1 = $("<h1 class='emptyListHeading'>No Warning Meters</h1>");
+			$("#warningTableBody").append(h1);
+		} else {
+			for (var i=0; i<warningMeters.length; i++) {
+				var row = $("<tr class='officialItem' data-href='warningMeter.html'><td data-th='Address'>" + warningMeters[i].name +"</td><td data-th='distance'>" + warningMeters[i].distance + " mi</td></tr>");
+				$("#warningTableBody").append(row);
+			}
+		}
+	}
+
 	$(document).ready(function () {
 		retrieveDataAndPopulate();
-		populateTable();
-		$(".officialItem").click(function (event) {
-			var $id = event.currentTarget.id;
-			var listOfItems = [{'a':2, 'b': 3}];
-			sessionStorage.setItem('clickedRow', $id);
+		// setInterval(retrieveDataAndPopulate, 60000);
+		$("#expiredTableBody").on('click', '.officialItem', function (event) {
+			var $index = $(this).index();
+			sessionStorage.setItem('selectedExpiredMeter', JSON.stringify(listOfExpiredMeters[$index]));
+			window.location = $(this).data("href");
+		});
+
+		$("#warningTableBody").on('click', '.officialItem', function (event) {
+			var $index = $(this).index();
+			sessionStorage.setItem('selectedWarningMeter', JSON.stringify(listOfWarningMeters[$index]));
 			window.location = $(this).data("href");
 		});
 	});
